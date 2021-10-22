e89eecbd0bf3196d22a407bab4b50c276f7fb581
Roscoe <elysiaroscoe@gmail.com>
2021-10-21 19:11:56 -0700

    inital

---
+++ script.js
@@ -0,0 +1,11 @@
+function alwaysHungry(arr) {
+    for (var i=0; i<alwaysHungry.length-1; i++)
+
+}
+
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+
