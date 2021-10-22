1070150639df37bed650c09ce5dc1e2d9c1be6f3
Roscoe <elysiaroscoe@gmail.com>
2021-10-21 19:36:21 -0700

    first 2

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 function alwaysHungry(arr) {
-    for (var i=0; i<alwaysHungry.length-1; i++)
-
+    var foodCount = 0;
+    for (var i=0; i<arr.length; i++){
+        if (arr[i] == "food"){
+            console.log("yummy");
+            foodCount++;
+        }
+    }
+    if(foodCount == 0){
+        console.log("I'm Hungry")
+    }
 }
 
 alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
+
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for (var i=0; i<arr.length; i++){
+        if (arr[i] > cutoff){
+            filteredArr.push(arr[i]); 
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
+
 
 
